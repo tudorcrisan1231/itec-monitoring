e3bdd1dbc81ef86bcac747d043184aad29ae4c47
tudorcrisan1231 <crisantudor79@yahoo.com>
2024-04-07 00:16:35 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
         <div class="grid grid-cols-1 lg:grid-cols-2 min-h-screen">
             <div class="flex items-center justify-center px-4 py-10 bg-white sm:px-6 lg:px-8 sm:py-16 lg:py-24">
                 <div class="xl:w-full xl:max-w-sm 2xl:max-w-md xl:mx-auto">
-                    <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign in to ItecMonitor</h2>
+                    <h2 class="text-3xl font-bold leading-tight text-black sm:text-4xl">Sign in to ItecMonitor1111</h2>
                     <p class="mt-2 text-base text-gray-600">Start for free today <a href="{{route('register')}}" title="" class="font-medium text-lime-600 transition-all duration-200 hover:text-lime-700 hover:underline focus:text-lime-700">Create an account</a></p>
 
                     <form action="{{route('login.custom')}}" method="POST" class="mt-8">
