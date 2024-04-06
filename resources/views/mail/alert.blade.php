40a9144fae3cbb0d7cb8ae6c82812ad7f1174b96
tudorcrisan1231 <crisantudor79@yahoo.com>
2024-04-06 22:53:48 +0300

    small update

--- resources/views/mail/alert.blade.php
+++ resources/views/mail/alert.blade.php
@@ -2,5 +2,5 @@
 
 @section('content')
     <h1>{{$alert_message}}</h1>
-    <p>{{$application->name}} has some problems</p>
+    <p>"<b>{{$application->name}}</b>" has some problems</p>
 @endsection
