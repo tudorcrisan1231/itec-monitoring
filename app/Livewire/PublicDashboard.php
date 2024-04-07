43550dbfd60735081557b9c6dd3d24eed679ab7c
tudorcrisan1231 <crisantudor79@yahoo.com>
2024-04-07 04:45:09 +0300

    Update PublicDashboard.php

--- app/Livewire/PublicDashboard.php
+++ app/Livewire/PublicDashboard.php
@@ -39,7 +39,31 @@ class PublicDashboard extends Component
         $this->selectedApplication->status = 'unstable';
         $this->selectedApplication->save();
 
-        SlackAlert::message("Someone reported an issue with your application. Please check the dashboard for more details. Message: $this->issue");
+        //curl request to: curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://hooks.slack.com/services/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',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => '{"text":"Someone reported an issue with your application. Please check the dashboard for more details. Message: '.$this->issue.'"}',
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/json'
+            ),
+        ));
+
+        $response = curl_exec($curl);
+        $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        curl_close($curl);
+
+        //dd response and status
+        dd($response, $status);
 
         Mail::to($this->selectedApplication->user ? $this->selectedApplication->user->email : '')->cc(ccMails())->queue(new AlertDeveloper($this->selectedApplication, "Someone reported an issue with your application. Please check the dashboard for more details. Message: $this->issue"));
 
