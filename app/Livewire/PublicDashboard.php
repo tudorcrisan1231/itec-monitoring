d3fa2c777308d0ae3d1ddb9e9cbb90f3f95ab1d0
tudorcrisan1231 <crisantudor79@yahoo.com>
2024-04-07 04:30:27 +0300

    Update PublicDashboard.php

--- app/Livewire/PublicDashboard.php
+++ app/Livewire/PublicDashboard.php
@@ -39,24 +39,24 @@ class PublicDashboard extends Component
         $this->selectedApplication->status = 'unstable';
         $this->selectedApplication->save();
 
-        Mail::to($this->selectedApplication->user ? $this->selectedApplication->user->email : '')->cc(ccMails())->queue(new AlertDeveloper($this->selectedApplication, "Someone reported an issue with your application. Please check the dashboard for more details. Message: $this->issue"));
-
         //make a post curl request to: curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
         $curl = curl_init();
         curl_setopt_array($curl, array(
-          CURLOPT_URL => env('SLACK_ALERT_WEBHOOK'),
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => "",
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => "POST",
-          CURLOPT_POSTFIELDS =>"{\"text\":\"Someone reported an issue with your application. Please check the dashboard for more details. Message: $this->issue\"}",
-          CURLOPT_HTTPHEADER => array(
-            "Content-type: application/json"
-          ),
+            CURLOPT_URL => env('SLACK_ALERT_WEBHOOK'),
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS =>"{\"text\":\"Someone reported an issue with your application. Please check the dashboard for more details. Message: $this->issue\"}",
+            CURLOPT_HTTPHEADER => array(
+                "Content-type: application/json"
+            ),
         ));
+
+        Mail::to($this->selectedApplication->user ? $this->selectedApplication->user->email : '')->cc(ccMails())->queue(new AlertDeveloper($this->selectedApplication, "Someone reported an issue with your application. Please check the dashboard for more details. Message: $this->issue"));
 
         return redirect()->route('home')->with('success', 'Issue reported successfully');
     }
