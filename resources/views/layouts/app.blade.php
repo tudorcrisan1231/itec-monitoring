38d9d1db00c37dc60728ab57f12173ec3466af4d
tudorcrisan1231 <crisantudor79@yahoo.com>
2024-04-10 12:53:13 +0300

    google analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <title>ITEC Monitoring</title>
+
     @vite('resources/css/app.css')
     @livewireChartsScripts
     @livewireStyles
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FFX95EG2V1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-FFX95EG2V1');
+</script>
 <body>
     @include('components.session')
     @yield('content')
